0857c164e45418546947de0636425540ee2ac6c0
saurabh singh <saurabhsingh100605@gmail.com>
2026-01-01 20:17:55 +0530

    fix: use env-based backend API url for production

--- frontend/src/helper.js
+++ frontend/src/helper.js
@@ -1,2 +1,19 @@
-export const api_base_url =
-  import.meta.env.VITE_API_URL || "http://localhost:8000";
+
+
+> blogsphere-frontend@1.0.0 build
+> vite build
+
+vite v7.1.1 building for production...
+✓ 101 modules transformed.
+dist/index.html                     0.47 kB │ gzip:   0.30 kB
+dist/assets/logo-ixsIrWfl.png      23.18 kB
+dist/assets/hero-Cl7fD0jZ.gif   3,308.22 kB
+dist/assets/index-EVBRmahF.css     44.97 kB │ gzip:   8.24 kB
+dist/assets/index-sG0EhmtM.js   1,425.35 kB │ gzip: 379.21 kB
+
+(!) Some chunks are larger than 500 kB after minification. Consider:
+- Using dynamic import() to code-split the application
+- Use build.rollupOptions.output.manualChunks to improve chunking: https://rollupjs.org/configuration-options/#output-manualchunks
+- Adjust chunk size limit for this warning via build.chunkSizeWarningLimit.
+✓ built in 4.38s
+PS C:\Users\saura\OneDrive\Desktop\blogspherehub\BlogSphere-Hub\frontend> 
