0e569c481efedfefef689dee27cd471f5dfcc85b
saurabh singh <saurabhsingh100605@gmail.com>
2026-01-03 13:09:56 +0530

    blogs imag e storage in production

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // -------------------------------------------------------------------------
 // IMPROVED CORS CONFIGURATION
